eea46127d07ad90135c2d3f506037b581779a9a1
jlafontaine <jlafontaine@student.unimelb.edu.au>
2021-08-27 14:42:49 +1000

    initial submission

---
+++ project-1-skeleton/src/ShadowFlap.java
@@ -0,0 +1,31 @@
+import bagel.*;
+
+/**
+ * Skeleton Code for SWEN20003 Project 1, Semester 2, 2021
+ *
+ * Please filling your name below
+ * @author:
+ */
+public class ShadowFlap extends AbstractGame {
+
+    public ShadowFlap() {
+    }
+
+    /**
+     * The entry point for the program.
+     */
+    public static void main(String[] args) {
+        ShadowFlap game = new ShadowFlap();
+        game.run();
+    }
+
+    /**
+     * Performs a state update.
+     * allows the game to exit when the escape key is pressed.
+     */
+    @Override
+    public void update(Input input) {
+
+    }
+
+}
